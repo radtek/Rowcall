2f914f86ab1067306dde4259027a7c31ef31efea
Christoffer <Christoffer@DESKTOP-RNF12US>
2018-04-27 10:57:33 +0200

    dfdf

--- MomensoBackend/Controllers/TokensController.cs
+++ MomensoBackend/Controllers/TokensController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MomensoBackend.Data;
+using MomensoBackend.Models;
 using RowcallBackend.Models;
 using TokenGenerator;
 
@@ -16,17 +18,21 @@ namespace RowcallBackend.Controllers
     public class TokensController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager; 
 
-        public TokensController(ApplicationDbContext context)
+        public TokensController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager; 
         }
 
         // GET: api/Tokens
         [HttpGet]
-        public IEnumerable<Token> GetToken()
+        public async Task<IEnumerable<Token>> GetToken()
         {
-            return _context.Token;
+            var currentUser = await _userManager.GetUserAsync(User);
+            var tokens = _context.Token.Include(x => x.UserToken).Where(x => x.UserToken.Any(p => p.ApplicationUserId == currentUser.Id)); 
+            return tokens;
         }
 
         // GET: api/Tokens/5
@@ -47,6 +53,26 @@ namespace RowcallBackend.Controllers
             };
 
             return Ok(token);
+        }
+
+        [HttpPost]
+        public IActionResult CreateToken([FromBody] TokenDto tokenDto)
+        {
+            WebService1SoapClient client = new WebService1SoapClient(
+                new BasicHttpBinding(BasicHttpSecurityMode.None),
+                new EndpointAddress("http://localhost/SOAPTokenGenerator/TokenGenerator.asmx")
+                );
+
+            Token token = new Token
+            {
+                Value = client.GenToken().ToString(),
+                Duration = 30,
+                CreatedDateTime = DateTime.Now,
+                ClassId = tokenDto.ClassId
+            };
+            _context.Token.Add(token);
+            _context.SaveChanges();
+            return Ok(token); 
         }
 
 
